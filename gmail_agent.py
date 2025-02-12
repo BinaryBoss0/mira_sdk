655740f745071104e11e2478a342d5fe9c5e80bd
BinaryBoss0 <ashutosh24114@iiitd.ac.in>
2025-02-12 16:07:56 +0530

    Update gmail_agent.py

--- gmail_agent.py
+++ gmail_agent.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+#!pip install composio_langchain
+#'pip install mira-sdk'
 
-# In[1]:
-
-
-get_ipython().system('pip install mira-sdk')
-
-
-# In[2]:
-
+get_ipython().system
 
 from mira_sdk import MiraClient
 client = MiraClient(config={"API_KEY": "mira_api_key"})
-
-
-# In[3]:
-
-
 MIRA_API_KEY="mira_api_key"
-
-
-# In[4]:
-
 
 from dotenv import load_dotenv
 from mira_sdk import MiraClient
@@ -31,23 +17,10 @@ load_dotenv()
 client = MiraClient(config={"API_KEY": os.getenv("MIRA_API_KEY")})
 
 
-# In[6]:
-
-
-#!pip install composio_langchain
-
-
-# In[5]:
-
-
 from langchain.agents import create_openai_functions_agent, AgentExecutor
 from langchain import hub
 from langchain_openai import ChatOpenAI
 from composio_langchain import ComposioToolSet, Action, App
-
-
-# In[11]:
-
 
 llm = ChatOpenAI(
     model="gpt-4o-mini",
@@ -58,10 +31,6 @@ llm = ChatOpenAI(
     api_key = "your_key",
 )
 
-
-# In[9]:
-
-
 prompt = hub.pull("hwchase17/openai-functions-agent")
 
 composio_toolset = ComposioToolSet(api_key="px6xqpbep7510sjdhdii5")
@@ -71,16 +40,8 @@ agent = create_openai_functions_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
-# In[13]:
-
-
-task = "send a mail to suhail@iiitd.ac.in , with subject lunch?, ask suhail if he wanna join me a lunch at 12:30pm"
+task = "your prompt/request Example: send a mail to abc@gmail.com , with subject lunch?, ask suhail if he wanna join me a lunch at 12:30pm"
 result = agent_executor.invoke({"input": task})
 print(result)
-
-
-# In[ ]:
-
-
 
 
